274a85c1afcf473a52f2cf3bff24f0754edfcb52
bijayabc <bijayabc1234@gmail.com>
2024-10-02 22:20:17 -0400

    added view engines and middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,8 +17,17 @@ mongoose.connect(dbURI)
     })
     .catch(err => (console.log(err)))
 
+// register view engine
+app.set('view engine', 'ejs')
+
+// middleware and static files
+app.use(express.static('public')) // to allow the browser to access the local filesystem i.e the public folder
+app.use(express.urlencoded({ extended: true })); // Middleware to parse form data
+app.use(morgan('dev')) // log the nature of the request to the console
+app.use(express.json()); // Middleware to parse JSON bodies
+
 app.get("/", (req, res) => { 
-    res.send("Express on Vercel"); 
+    res.send("Express on Vercel after link to mongoDB"); 
 });
 
 module.exports = app;
