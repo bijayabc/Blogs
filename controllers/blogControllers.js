96bd05f123a12713733175a5b48b0214a71772b0
bijayabc <bijayabc1234@gmail.com>
2024-10-02 22:42:57 -0400

    added all ejs files and css file

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,15 +1,12 @@
 const Blog = require('../models/blog')
 
 const blog_index = (req, res) => {
-    const blogs = Blog.find()
-    res.send('hi')
-
-    // sort the lists based on the time of creations
-    // const blogs = Blog.find().sort( {createdAt: -1} )
-    //     .then((result) => {
-    //         res.render('index.ejs', { title: 'All Blogs', blogs: result})
-    //     })
-    //     .catch(err => console.log(err))
+    // sort the lists based on the time of creation
+    const blogs = Blog.find().sort( {createdAt: -1} )
+        .then((result) => {
+            res.render('index.ejs', { title: 'All Blogs', blogs: result})
+        })
+        .catch(err => console.log(err))
 }
 
 module.exports = {
