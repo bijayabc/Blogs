e43b85884b1c95faf311b81438ec5a77dedede0b
bijayabc <bijayabc1234@gmail.com>
2024-10-02 22:15:40 -0400

    Linked to mongoDB

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => { 
-    res.send("Express on Vercel"); 
-});
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => { 
-    console.log(`Server is running on port ${PORT}`); 
+// app.listen(PORT, () => { 
+//     console.log(`Server is running on port ${PORT}`); 
+// });
+
+// connect to database
+const dbURI = 'mongodb+srv://bijayabc:DRDDkUnvnofOsyCC@node-app.ju9ze.mongodb.net/blogs?retryWrites=true&w=majority&appName=node-app'
+mongoose.connect(dbURI)
+    .then((result) => {
+        app.listen(3000, () => console.log(`Server is running on port ${PORT}`));
+    })
+    .catch(err => (console.log(err)))
+
+app.get("/", (req, res) => { 
+    res.send("Express on Vercel"); 
 });
 
 module.exports = app;
